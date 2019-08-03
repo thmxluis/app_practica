b7b108e81722864ab300f6d76b6b87c960eb2ca6
Luis Gonzalez <luis.gonzalez@orbitec.pe>
2019-08-03 12:16:08 -0500

    practica 1

--- config/settings/base.py
+++ config/settings/base.py
@@ -143,7 +143,7 @@ EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.s
 # Admin
 ADMIN_URL = 'admin/'
 ADMINS = [
-    ("""Pablo Trinidad""", 'pablotrinidad@ciencias.unam.mx'),
+    ("""Luis Gonzalez""", 'gonzalez.g.luis.m@gmail.com'),
 ]
 MANAGERS = ADMINS
 
